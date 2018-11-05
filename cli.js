a0ef547bfd4f2c5de3015931e2c5b5f255aacc66
ugate <ugate.public@gmail.com>
2018-11-05 10:07:47 -0500

    [NOP]: Removed unnecessary pages.js script (combined in cli.js)

--- cli.js
+++ cli.js
@@ -1,12 +1,30 @@
 #!/usr/bin/env node
 
+/**
+ * Command line `cli` for `jspub`.
+ * <br/>__Command Line Options:__
+ * - `-D` (or `--deploy`): Attempts to deploy the generated `jsdoc` to the configured deployment branch
+ * @example
+ * jspub ./jspub/jsdoc.conf
+ * @example
+ * jspub -D ./jspub/jsdoc.conf
+ * @example
+ * jspub --deploy ./jspub/jsdoc.conf
+ * @module jspub
+ */
+
 const jspub = require('./index');
 
 const argv = process.argv.slice(2);
-if (!argv.length || !argv[0]) throw new Error('The 1st argument must be a path to the jspub options JSON file');
+var deploy, confPth;
+for (let arg of argv) {
+  if (arg.toUpperCase() === '-D' || arg.toUpperCase() === '--deploy') deploy = true;
+  else if (!confPth) confPth = arg;
+}
+if (!confPth) throw new Error('A path to the jspub options JSON file must be present');
 (async () => {
   try {
-    await jspub(argv[0]);
+    await jspub(confPth, deploy);
   } catch (err) {
     console.error(err);
     process.exit(1);
